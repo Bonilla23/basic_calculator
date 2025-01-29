bdaa10d64e0c7c7d5cbb9ba4cb9803cdd2c4df8b
Bonilla23 <ikerfernandezbonilla@gmail.com>
2025-01-29 09:13:50 +0100

    Create the Menu and the bucle

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -1,7 +1,20 @@
 # Create the variables
 num1 = 0
 num2 = 0
+salir = "Y"
 
 # Ask the numbers
 num1 = int(input("First number: "))
 num2 = int(input("Second number: "))
+
+# Create the bucle
+while salir == "Y":
+    #---Menu----#
+    print("----Menu----")
+    print("Sumar (" ,num1, "+ " ,num2, ")")
+    print("Restar (" ,num1, "- " ,num2, ")")
+    print("Multiplicar (" ,num1, "* " ,num2, ")")
+    print("Dividir (" ,num1, "/ " ,num2, ")")
+    # Variable option
+    option = str(input("Introduce una opcion: "))
+    
